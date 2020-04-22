10fcc8cf62cd7022951897c585371d6fafa20f7e
Eduardo Andrade <eduardo.ufg@hotmail.com>
2020-04-22 15:56:42 -0300

    git

---
+++ app/javascript/screens/discovery/index.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+const DiscoveryScreen = {} =>{
+    return{
+        <Fragment>
+            <div> Home Screen</div>
+        </Fragment>
+    }
+}
+
+export default DiscoveryScreen
---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
+      
+  concern :favoritable do |options|
+    shallow do
+        post 'favorite', { to: "favorites#create", on: :member }.merge(options)
+        delete 'favorite', { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+
+  namespace :api, defaults: {format: JSON } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only:[ :index, :show]
+      resources :search, only: :index
+      resources :albums, only: :show do
+        concerns :favoritable, favoritable_type:'Album'
+        resources :recently_heards, only: :create
+      end
+      resources :favorites, only: :index
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type:'Song'
+      end
+      resources :artist, only: [] do
+        concerns :favoritable, favoritable_type:'Artist'
+      end
+    end
+  end
+end
